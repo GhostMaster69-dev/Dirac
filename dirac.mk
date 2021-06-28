88bcb5bd080a139a8da847ed9137a3d43235caa7
Mocarafee <harif.rp05@gmail.com>
2021-06-28 17:54:13 +0700

    dirac: define VENDOR_PATH as DIRAC_VENDOR_PATH
    * To avoid conflict with MiCam VENDOR_PATH

--- dirac.mk
+++ dirac.mk
@@ -1,19 +1,19 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/dirac
 
-VENDOR_PATH := vendor/dirac
+DIRAC_VENDOR_PATH := vendor/dirac
 
 PRODUCT_COPY_FILES += \
-    $(VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
-    $(VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
-    $(VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
-    $(VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
+    $(DIRAC_VENDOR_PATH)/vendor/lib/libDiracAPI_SHARED.so:$(TARGET_COPY_OUT_VENDOR)/lib/libDiracAPI_SHARED.so \
+    $(DIRAC_VENDOR_PATH)/vendor/lib/soundfx/libdirac.so:$(TARGET_COPY_OUT_VENDOR)/lib/soundfx/libdirac.so \
+    $(DIRAC_VENDOR_PATH)/vendor/etc/diracmobile.config:$(TARGET_COPY_OUT_VENDOR)/etc/diracmobile.config \
+    $(DIRAC_VENDOR_PATH)/vendor/etc/diracvdd.bin:$(TARGET_COPY_OUT_VENDOR)/etc/diracvdd.bin
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DIRAC_VENDOR_PATH)/sepolicy
 
 # Dirac
 PRODUCT_PACKAGES += \
     XiaomiDirac \
 
 # Props ($PATH_PROP)
-TARGET_PRODUCT_PROP += $(VENDOR_PATH)/dirac.prop
+TARGET_PRODUCT_PROP += $(DIRAC_VENDOR_PATH)/dirac.prop
